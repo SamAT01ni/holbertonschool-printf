adb68b32f7640f8f62d5867911fcbc9b662af9f7
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-03-19 05:21:45 +0000

    header file and function for %d and %i

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _printf(cost char *format, ...);
+int printf_int(int n);
+
+#endif
